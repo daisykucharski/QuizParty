e86fb9fe8ab71c684157b41c868a2a2aa0c5b14d
Daisy Kucharski <daisy.kucharski@gmail.com>
2021-10-19 14:57:20 -0400

    Added API endpoints

--- server/index.js
+++ server/index.js
@@ -7,65 +7,78 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  try {
-    const categories = await pool.query(
-      "SELECT * FROM category WHERE round_name = 'Jeopardy!' ORDER BY random() LIMIT 5"
-    );
-
-    res.json(categories.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-app.get("/jeopardy", async (req, res) => {
-  try {
-    // Get a random jeopardy round board from pool
-  } catch (error) {
-    console.error(error.message);
-  }
-});
+app.get("/jeopardy", async (req, res) => getNormalRound(res, "Jeopardy!", 0));
 
 app.get("/jeopardy/:year", async (req, res) => {
-  try {
-    // Get a random jeopardy round board from pool where the year of the airdate is >= year
-  } catch (error) {
-    console.error(error.message);
-  }
+    const { year } = req.params;
+    getNormalRound(res, "Jeopardy!", year)
 });
 
-app.get("/doublejeopardy", async (req, res) => {
-  try {
-    // Get a random double jeopardy round board from pool
-  } catch (error) {
-    console.error(error.message);
-  }
-});
+app.get("/doublejeopardy", async (req, res) => getNormalRound(res, "Double Jeopardy!", 0));
 
 app.get("/doublejeopardy/:year", async (req, res) => {
-  try {
-    // Get a random double jeopardy round board from pool where the year of the airdate is >= year
-  } catch (error) {
-    console.error(error.message);
-  }
+  const { year } = req.params;
+  getNormalRound(res, "Double Jeopardy!", year);
 });
 
-app.get("/finaljeopardy", async (req, res) => {
-  try {
-    // Get a random final jeopardy question from pool
-  } catch (error) {
-    console.error(error.message);
-  }
-});
+app.get("/finaljeopardy", async (req, res) =>
+  getFinalJeopardyRound(res, 0)
+);
 
 app.get("/finaljeopardy/:year", async (req, res) => {
+  const { year } = req.params;
+  getFinalJeopardyRound(res, year);
+});
+
+// Responds to a requesut to get a given round type with the given earliest year
+async function getNormalRound(res, round_name, earliest_year) {
   try {
-    // Get a random final jeopardy round question  from pool where the year of the airdate is >= year
+    const selectedCategories = await pool.query(
+      "SELECT * FROM category WHERE round_name = $1 AND EXTRACT(YEAR FROM air_date) > $2 ORDER BY random() LIMIT 5",
+      [round_name, earliest_year]
+    );
+
+    const categoryRows = selectedCategories.rows;
+    const result = [];
+
+    for (i = 0; i < 5; i++) {
+      const category = categoryRows[i];
+      const categoryResult = { category: category.category };
+      let questions = await pool.query(
+        "SELECT question_value, question, answer FROM question WHERE category_id = $1",
+        [category.category_id]
+      );
+      questions = questions.rows;
+      categoryResult.questions = questions;
+      result.push(categoryResult);
+    }
+
+    res.json({ categories: result });
   } catch (error) {
     console.error(error.message);
   }
-});
+}
+
+// Responds to a request to get a final jeopardy round question with the given earliest year
+async function getFinalJeopardyRound(res, earliest_year) {
+  try {
+    const selectedCategory = await pool.query(
+      "SELECT * FROM category WHERE round_name = 'Final Jeopardy!' AND EXTRACT(YEAR FROM air_date) > $1 ORDER BY random() LIMIT 1",
+      [earliest_year]
+    );
+    const category = selectedCategory.rows[0];
+    const result = { category: category.category };
+    const question = await pool.query(
+      "SELECT question, answer FROM question WHERE category_id = $1",
+      [category.category_id]
+    );
+    result.question = question.rows[0].question;
+    result.answer = question.rows[0].answer;
+    res.json(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
