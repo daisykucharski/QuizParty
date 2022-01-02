3976a1911535e37f390fe34891893b2d85ad042f
Daisy Kucharski <daisy.kucharski@gmail.com>
2022-01-01 21:23:12 -0500

    Added kicking players

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import Game from "./game";
+import { Player } from "./types";
 import { generateRandomCode } from "./utilities";
 import { Socket } from "socket.io";
 const app = require("express")();
@@ -44,15 +45,8 @@ io.on("connection", (socket: Socket) => {
     }
   });
 
-  //
-  socket.on("newRoomJoin", ({ room, name }: { room: string; name: string }) => {
-    // If someone goes to the game page without a room or name, then redirect
-    // them back to the starting page
-    if (room === "" || name === "") {
-      io.to(socket.id).emit("joinError");
-      return;
-    }
-
+  // When the display client joins, add them to the room if it exists
+  socket.on("newDisplayJoin", ({ room }) => {
     const game = rooms.get(room);
 
     // if the room doesn't exist, then redirect back to starting page
@@ -61,15 +55,50 @@ io.on("connection", (socket: Socket) => {
       return;
     }
 
-    // If there are already 6 players in the game, then redirect back to starting page
-    if (game.getNumPlayers() === 6) {
-      io.to(socket.id).emit("joinError");
+    socket.join(room);
+  });
+
+  // When a new player tries to join a game room, either add them to room they're trying
+  // to join or redirect them to the starting page
+  socket.on(
+    "newPlayerJoin",
+    ({ room, name }: { room: string; name: string }) => {
+      // If someone goes to the game page without a room or name, then redirect
+      // them back to the starting page
+      if (room === "" || name === "") {
+        io.to(socket.id).emit("joinError");
+        return;
+      }
+
+      const game = rooms.get(room);
+
+      // if the room doesn't exist, then redirect back to starting page
+      if (!game) {
+        io.to(socket.id).emit("joinError");
+        return;
+      }
+
+      // If there are already 6 players in the game, then redirect back to starting page
+      if (game.getPlayers().length === 6) {
+        io.to(socket.id).emit("joinError");
+        return;
+      }
+
+      socket.join(room);
+      const newPlayer = { name, earnings: 0, socketid: socket.id };
+      game.addPlayer(newPlayer);
+      io.to(room).emit("newPlayers", { players: game.getPlayers() });
+    }
+  );
+
+  // Kicks the given player from the room
+  socket.on("kick", ({ room, player }: { room: string; player: Player }) => {
+    const game = rooms.get(room);
+    if (!game) {
       return;
     }
-
-    socket.join(room);
-    const newPlayer = { name, earnings: 0, socketid: socket.id };
-    game.addPlayer(newPlayer);
+    game.removePlayer(player);
+    io.to(room).emit("newPlayers", { players: game.getPlayers() });
   });
 
   // When the client requests the game to start, start the next round and emit the starting data
